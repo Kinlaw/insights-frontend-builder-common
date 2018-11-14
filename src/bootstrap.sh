f7a72d7ae6f5cad43db846e0f9738bc330ffbc82
Josh Kinlaw <jkinlaw@redhat.com>
2018-11-14 15:05:45 -0500

    Update bootstrap.sh

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-COMMON_URL="https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master/"
+COMMON_URL="https://raw.githubusercontent.com/Kinlaw/insights-frontend-builder-common/master/"
 curl $COMMON_URL/src/after_success.sh > .travis/after_success.sh
 curl $COMMON_URL/src/release.sh > .travis/release.sh
 curl $COMMON_URL/src/Jenkinsfile > .travis/58231b16fdee45a03a4ee3cf94a9f2c3
